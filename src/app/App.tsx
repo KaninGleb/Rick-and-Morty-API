97fd323c7eebecea66d86c2029aea2f929209170
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-16 22:19:11 +0300

    Routes added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,17 @@
 import { Header } from '@/common'
 import { HomePage, CharacterPage, LocationPage, EpisodePage } from '@/pages'
+import { Routes, Route } from 'react-router'
 
 export const App = () => {
   return (
     <div>
       <Header />
-      <HomePage />
-      <CharacterPage />
-      <LocationPage />
-      <EpisodePage />
+      <Routes>
+        <Route path={PATH.Home} element={<HomePage />} />
+        <Route path={PATH.Characters} element={<CharacterPage />} />
+        <Route path={PATH.Locations} element={<LocationPage />} />
+        <Route path={PATH.Episodes} element={<EpisodePage />} />
+      </Routes>
     </div>
   )
 }
