2786fc4fe506f349d2c42b2de60a8f5f6cd6a031
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-16 22:27:39 +0300

    HomePage - NavLinks added

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,15 @@
+import { NavLink } from 'react-router'
+import { PATH } from '@/app/App.tsx'
+
 export const HomePage = () => {
   return (
     <div>
       <h1>The Rick and Morty</h1>
+      <nav>
+        <NavLink to={PATH.Characters}>Characters</NavLink>
+        <NavLink to={PATH.Locations}>Locations</NavLink>
+        <NavLink to={PATH.Episodes}>Episodes</NavLink>
+      </nav>
     </div>
   )
 }
