26d3882635962122f210e89a365b8b9f031746e5
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-16 22:35:36 +0300

    Header - NavLinks added

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -1,9 +1,17 @@
 import { logo } from '@/assets'
+import { NavLink } from 'react-router'
+import { PATH } from '@/app/App.tsx'
 
 export const Header = () => {
   return (
     <div>
       <img src={logo} alt="logotype" />
+      <nav>
+        <NavLink to={PATH.Home}>Home</NavLink>
+        <NavLink to={PATH.Characters}>Characters</NavLink>
+        <NavLink to={PATH.Locations}>Locations</NavLink>
+        <NavLink to={PATH.Episodes}>Episodes</NavLink>
+      </nav>
     </div>
   )
 }
