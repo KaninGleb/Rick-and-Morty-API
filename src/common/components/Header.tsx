a6dbe838165f6f8b5085664ca2fdb7fa8312c45c
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-16 22:37:00 +0300

    Header - logotype wrapped with NavLink

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -5,7 +5,9 @@ import { PATH } from '@/app/App.tsx'
 export const Header = () => {
   return (
     <div>
-      <img src={logo} alt="logotype" />
+      <NavLink to={PATH.Home}>
+        <img src={logo} alt="logotype" />
+      </NavLink>
       <nav>
         <NavLink to={PATH.Home}>Home</NavLink>
         <NavLink to={PATH.Characters}>Characters</NavLink>
