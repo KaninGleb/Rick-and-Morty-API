0baf1a79d422d2095f555b2ebdc4bc21376a205d
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-21 23:00:09 +0300

    Styles for LocationPage implemented

--- src/pages/LocationPage/LocationPage.tsx
+++ src/pages/LocationPage/LocationPage.tsx
@@ -1,7 +1,9 @@
+import s from './LocationPage.module.css'
+
 export const LocationPage = () => {
   return (
     <div>
-      <h1>LocationPage</h1>
+      <h1 className={`pageTitle ${s.title}`}>LocationPage</h1>
     </div>
   )
 }
