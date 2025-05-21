d1e61de951c81ed71825f687653eb16910147b78
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-21 22:58:49 +0300

    Styles for HomePage implemented

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,21 @@
 import { NavLink } from 'react-router'
 import { PATH } from '@/app/App.tsx'
+import s from './HomePage.module.css'
 
 export const HomePage = () => {
   return (
-    <div>
-      <h1>The Rick and Morty</h1>
-      <nav>
-        <NavLink to={PATH.Characters}>Characters</NavLink>
-        <NavLink to={PATH.Locations}>Locations</NavLink>
-        <NavLink to={PATH.Episodes}>Episodes</NavLink>
+    <div className={s.mainWrapper}>
+      <h1 className={`pageTitle ${s.title}`}>The Rick and Morty</h1>
+      <nav className={s.linkWrapper}>
+        <NavLink to={PATH.Characters} className={'linkButton'}>
+          Characters
+        </NavLink>
+        <NavLink to={PATH.Locations} className={'linkButton'}>
+          Locations
+        </NavLink>
+        <NavLink to={PATH.Episodes} className={'linkButton'}>
+          Episodes
+        </NavLink>
       </nav>
     </div>
   )
