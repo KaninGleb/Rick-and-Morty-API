66baafcf7ac36779cf86db818facdfe573cd4b1d
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-21 23:00:22 +0300

    Styles for Header implemented

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -1,18 +1,27 @@
 import { logo } from '@/assets'
 import { NavLink } from 'react-router'
 import { PATH } from '@/app/App.tsx'
+import s from './Header.module.css'
 
 export const Header = () => {
   return (
-    <div>
+    <div className={s.container}>
       <NavLink to={PATH.Home}>
-        <img src={logo} alt="logotype" />
+        <img className={s.logo} src={logo} alt="logotype" />
       </NavLink>
       <nav>
-        <NavLink to={PATH.Home}>Home</NavLink>
-        <NavLink to={PATH.Characters}>Characters</NavLink>
-        <NavLink to={PATH.Locations}>Locations</NavLink>
-        <NavLink to={PATH.Episodes}>Episodes</NavLink>
+        <NavLink to={PATH.Home} className={s.headerLink}>
+          Home
+        </NavLink>
+        <NavLink to={PATH.Characters} className={s.headerLink}>
+          Characters
+        </NavLink>
+        <NavLink to={PATH.Locations} className={s.headerLink}>
+          Locations
+        </NavLink>
+        <NavLink to={PATH.Episodes} className={s.headerLink}>
+          Episodes
+        </NavLink>
       </nav>
     </div>
   )
