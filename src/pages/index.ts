4dca5b1d60d2987df600f0a74c27b8a371aa53fa
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-24 16:27:58 +0300

    index.ts - api and response types exported

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,6 @@ export { CharacterPage } from './CharacterPage/CharacterPage'
 export { EpisodePage } from './EpisodePage/EpisodePage'
 export { HomePage } from './HomePage/HomePage'
 export { LocationPage } from './LocationPage/LocationPage'
+
+export { api } from './api/api'
+export * from './api/api.types'
