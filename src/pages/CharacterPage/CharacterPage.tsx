5b1f969f0355ad6ea923ddc719b251514b780f43
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-24 16:28:40 +0300

    CharacterPage - api logic added

--- src/pages/CharacterPage/CharacterPage.tsx
+++ src/pages/CharacterPage/CharacterPage.tsx
@@ -1,9 +1,31 @@
+import { useState, useEffect } from 'react'
+import { api, type Results } from '@/pages'
 import s from './CharacterPage.module.css'
 
 export const CharacterPage = () => {
+  const [characters, setCharacters] = useState<Results[]>([])
+
+  useEffect(() => {
+    api.getCharacters().then((res) => {
+      setCharacters(res.data.results)
+    })
+  }, [])
+
   return (
     <div>
-      <h1 className={`pageTitle ${s.title}`}>CharacterPage</h1>
+      <h1 className={'pageTitle'}>CharacterPage</h1>
+      <div>
+        {characters.length && (
+          <div className={s.characters}>
+            {characters.map((char) => (
+              <div key={char.id} className={s.character}>
+                <div className={s.characterLink}>{char.name}</div>
+                <img src={char.image} alt={`${char.name} avatar`} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
