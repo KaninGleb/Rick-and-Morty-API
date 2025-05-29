e2a5346fa9044ebd903f454371101e6171c57af0
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-30 00:49:25 +0300

    App - Routes supplemented with Character Route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from '@/common'
-import { HomePage, CharacterPage, LocationPage, EpisodePage } from '@/pages'
+import { HomePage, LocationPage, EpisodePage } from '@/pages'
+import { CharacterPage, Character } from '@/pages/CharacterPage'
 import { Routes, Route } from 'react-router'
 
 export const PATH = {
   Home: '/',
   Characters: '/characters',
+  Character: '/characters/:id',
   Locations: '/locations',
   Episodes: '/episodes',
 } as const
@@ -16,6 +18,7 @@ export const App = () => {
       <Routes>
         <Route path={PATH.Home} element={<HomePage />} />
         <Route path={PATH.Characters} element={<CharacterPage />} />
+        <Route path={PATH.Character} element={<Character />} />
         <Route path={PATH.Locations} element={<LocationPage />} />
         <Route path={PATH.Episodes} element={<EpisodePage />} />
       </Routes>
