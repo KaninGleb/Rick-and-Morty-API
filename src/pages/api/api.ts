3c28e6437022c4687dfd1f21a8daab62d8280c23
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-30 23:12:42 +0300

    api - getLocations added

--- src/pages/api/api.ts
+++ src/pages/api/api.ts
@@ -1,8 +1,11 @@
 import { instance } from '@/common'
-import type { BaseResponse, CharactersResults } from '@/pages/api/api.types.ts'
+import type { BaseResponse, CharactersResults, LocationResults } from '@/pages/api/api.types.ts'
 
 export const api = {
-  getCharacters(url: string = '/character') {
+  getCharacters(url: string = `/character`) {
     return instance.get<BaseResponse<CharactersResults>>(url)
+  },
+  getLocations() {
+    return instance.get<BaseResponse<LocationResults>>(`/location`)
   },
 }
