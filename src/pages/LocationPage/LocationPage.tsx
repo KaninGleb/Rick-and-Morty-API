7b65d8185c9b029b73b0469f8add94aec16287b6
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-30 23:12:52 +0300

    LocationPage - data visualized

--- src/pages/LocationPage/LocationPage.tsx
+++ src/pages/LocationPage/LocationPage.tsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from 'react'
+import { api, type LocationResults } from '@/pages/api'
 import s from './LocationPage.module.css'
 
 export const LocationPage = () => {
+  const [locations, setLocations] = useState<LocationResults[]>([])
+
+  useEffect(() => {
+    api.getLocations().then((res) => {
+      setLocations(res.data.results)
+    })
+  }, [])
+
   return (
-    <div>
-      <h1 className={`pageTitle ${s.title}`}>LocationPage</h1>
+    <div className={'pageContainer'}>
+      <div>
+        <h1 className={`pageTitle ${s.title}`}>LocationPage</h1>
+        <div>
+          {locations.map((l) => {
+            return (
+              <div key={l.id}>
+                <ul>
+                  <li>
+                    Location name: <b>{l.name}</b>
+                  </li>
+                  <li>
+                    Location type: <b>{l.type}</b>
+                  </li>
+                  <li>
+                    Measurement where the location is: <b>{l.dimension}</b>
+                  </li>
+                  <li>
+                    Number of characters seen in this location: <b>{l.residents.length}</b>
+                  </li>
+                </ul>
+                <hr />
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </div>
   )
 }
