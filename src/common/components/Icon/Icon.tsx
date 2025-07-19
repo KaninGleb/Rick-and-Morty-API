d23ce5a11bbeb7c0f5c68af0d049d8ec0b9b4ffb
Kanin Gleb <gleb.kanin@gmail.com>
2025-07-19 21:40:20 +0300

    refactor(error): update error icon

--- src/common/components/Icon/Icon.tsx
+++ src/common/components/Icon/Icon.tsx
@@ -17,9 +17,9 @@ export const Icon = ({ name, width = 24, height = 24, ...props }: IconProps) => 
 
     error: (
       <svg width={width} height={height} viewBox="0 0 24 24" fill="none" stroke="currentColor" aria-hidden="true" {...props}>
-        <circle cx="12" cy="12" r="10" />
-        <line x1="12" y1="8" x2="12" y2="12" />
-        <line x1="12" y1="16" x2="12.01" y2="16" />
+        <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M12 8V12" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M12 16.01L12.01 15.9989" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
     ),
 
