2201ac99f81fefcd0b7fa43fcbc06dc449e84829
Kanin Gleb <gleb.kanin@gmail.com>
2025-07-26 17:16:17 +0300

    feat(Character): improve component logic

--- src/pages/CharacterPage/Character/Character.tsx
+++ src/pages/CharacterPage/Character/Character.tsx
@@ -1,52 +1,35 @@
-import { useParams, Link } from 'react-router'
-import { useState, useEffect } from 'react'
-import { instance, getEpisodeId } from '@/common'
-import { Loader } from '@/common/components'
-import { getStatusClassName } from './CharacterHelpers.ts'
-import type { CharactersResults, ErrorType } from '@/pages/api'
+import { Link, useParams } from 'react-router'
+import { Loader, Icon } from '@/common/components'
 import { PATH } from '@/common/data/paths.ts'
+import { useFetchById } from '@/common/hooks/useFetchById.ts'
+import { getStatusClassName } from './CharacterHelpers.ts'
+import type { CharactersResults } from '@/pages/api'
+import { getEpisodeId } from '@/common'
 import s from './Character.module.css'
 
 export const Character = () => {
   const { id } = useParams()
-
-  const [character, setCharacter] = useState<CharactersResults | null>(null)
-  const [error, setError] = useState<ErrorType>(null)
+  const { data: character, error, isLoading } = useFetchById<CharactersResults>('/character', id)
 
   const infoFields = character
     ? [
         { title: 'Last known location', value: character.location.name ?? 'Unknown' },
-        { title: 'Origin', value: character.origin.name ?? 'Unknown' },
+        {
+          title: 'Origin',
+          value: character.origin.name
+            ? character.origin.name.charAt(0).toUpperCase() + character.origin.name.slice(1)
+            : 'Unknown',
+        },
         { title: 'Gender', value: character.gender ?? 'Unknown' },
         { title: 'Episodes', value: character.episode.length ?? 0 },
       ]
     : []
 
-  useEffect(() => {
-    const controller = new AbortController()
-
-    instance
-      .get(`/character/${id}`, { signal: controller.signal })
-      .then((res) => {
-        setCharacter(res.data)
-        setError(null)
-      })
-      .catch(() => {
-        if (!controller.signal.aborted) {
-          setError('Failed to fetch character details.')
-        }
-      })
-
-    return () => {
-      controller.abort()
-    }
-  }, [id])
-
   return (
     <div className={s.pageContainer}>
       {error && <div className={s.errorMessage}>{error}</div>}
 
-      {!character && !error && <Loader colorType={'characters'} text={'Loading character details...'} />}
+      {isLoading && <Loader colorType={'characters'} text={'Loading character details...'} />}
 
       {character && (
         <div className={s.container}>
@@ -58,7 +41,7 @@ export const Character = () => {
                 <div className={s.statusContainer}>
                   <span className={getStatusClassName(character.status)}></span>
                   <span className={s.statusText}>
-                    {character.status} - {character.species}
+                    {character.status} • {character.species}
                     {character.type && ` (${character.type})`}
                   </span>
                 </div>
@@ -72,19 +55,23 @@ export const Character = () => {
               </div>
             </div>
 
-            <div className={s.episodeList}>
-              {character.episode.map((episodeUrl) => {
-                const id = getEpisodeId(episodeUrl)
-                return (
-                  <Link key={id} to={`${PATH.Episodes}/${id}`} className={s.episodeLink}>
-                    Episode {id}
-                  </Link>
-                )
-              })}
-            </div>
+            {character.episode.length > 0 ? (
+              <div className={s.episodeList}>
+                {character.episode.map((episodeUrl) => {
+                  const id = getEpisodeId(episodeUrl)
+                  return (
+                    <Link key={id} to={`${PATH.Episodes}/${id}`} className={s.episodeLink}>
+                      Episode {id}
+                    </Link>
+                  )
+                })}
+              </div>
+            ) : (
+              <span className={s.noEpisodes}>No episodes available</span>
+            )}
           </div>
           <Link to={PATH.Characters} className={s.backButton}>
-            Back to Characters
+            <Icon name="previous" /> Back to Characters
           </Link>
         </div>
       )}
