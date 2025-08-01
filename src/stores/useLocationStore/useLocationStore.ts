84408cd13a32b9d1b587f48fc3e58d1582e91a11
Kanin Gleb <gleb.kanin@gmail.com>
2025-08-01 22:27:25 +0300

    refactor(useLocationStore): replace manual logic with createEntityStore

--- src/stores/useLocationStore/useLocationStore.ts
+++ src/stores/useLocationStore/useLocationStore.ts
@@ -1,64 +1,69 @@
-import { create } from 'zustand'
-import type { LocationZustandStore } from './useLocationStore.types.ts'
+import { createEntityStore } from '@/stores'
 import { api } from '@/pages/api'
-import type { AxiosError } from 'axios'
 
-export const useLocationStore = create<LocationZustandStore>((set, get) => ({
-  locations: [],
-  info: {
-    count: 0,
-    pages: 0,
-    next: null,
-    prev: null,
-  },
-  searchQuery: '',
-  isLoading: false,
-  error: null,
-  scrollPosition: 0,
+export const useLocationStore = createEntityStore(api.getLocations, '/location')
 
-  setSearchQuery: (query) => {
-    set({ searchQuery: query })
-  },
-
-  setScrollPosition: (position) => {
-    set({ scrollPosition: position })
-  },
-
-  fetchLocations: async (url = '/location') => {
-    set({ isLoading: true, error: null })
-    try {
-      const res = await api.getLocations(url)
-      set({
-        locations: res.data.results,
-        info: res.data.info,
-        isLoading: false,
-      })
-    } catch (e) {
-      const error = e as AxiosError
-      set({
-        error: error.message || 'Unknown error',
-        isLoading: false,
-        locations: [],
-        info: { count: 0, pages: 0, next: null, prev: null },
-      })
-    }
-  },
-
-  fetchNextPage: async () => {
-    const { info, locations } = get()
-    if (!info.next) return
-
-    set({ isLoading: true })
-    try {
-      const response = await api.getLocations(info.next)
-      set({
-        locations: [...locations, ...response.data.results],
-        info: response.data.info,
-        isLoading: false,
-      })
-    } catch (e) {
-      const message = (e as Error)?.message || 'Unknown error'
-      set({ error: message, isLoading: false })
-    }
-  },
-}))
+// import { create } from 'zustand'
+// import type { LocationZustandStore } from './useLocationStore.types.ts'
+// import { api } from '@/pages/api'
+// import type { AxiosError } from 'axios'
+//
+// export const useLocationStore = create<LocationZustandStore>((set, get) => ({
+//   locations: [],
+//   info: {
+//     count: 0,
+//     pages: 0,
+//     next: null,
+//     prev: null,
+//   },
+//   searchQuery: '',
+//   isLoading: false,
+//   error: null,
+//   scrollPosition: 0,
+//
+//   setSearchQuery: (query) => {
+//     set({ searchQuery: query })
+//   },
+//
+//   setScrollPosition: (position) => {
+//     set({ scrollPosition: position })
+//   },
+//
+//   fetchLocations: async (url = '/location') => {
+//     set({ isLoading: true, error: null })
+//     try {
+//       const res = await api.getLocations(url)
+//       set({
+//         locations: res.data.results,
+//         info: res.data.info,
+//         isLoading: false,
+//       })
+//     } catch (e) {
+//       const error = e as AxiosError
+//       set({
+//         error: error.message || 'Unknown error',
+//         isLoading: false,
+//         locations: [],
+//         info: { count: 0, pages: 0, next: null, prev: null },
+//       })
+//     }
+//   },
+//
+//   fetchNextPage: async () => {
+//     const { info, locations } = get()
+//     if (!info.next) return
+//
+//     set({ isLoading: true })
+//     try {
+//       const response = await api.getLocations(info.next)
+//       set({
+//         locations: [...locations, ...response.data.results],
+//         info: response.data.info,
+//         isLoading: false,
+//       })
+//     } catch (e) {
+//       const message = (e as Error)?.message || 'Unknown error'
+//       set({ error: message, isLoading: false })
+//     }
+//   },
+// }))
