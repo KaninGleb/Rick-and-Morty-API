4df2adb6bb5ad6c5dbf3c29963f55cd3e388c599
Kanin Gleb <gleb.kanin@gmail.com>
2025-08-01 22:55:07 +0300

    feat(LocationPage): implement EntityListPage

--- src/pages/LocationPage/LocationPage.tsx
+++ src/pages/LocationPage/LocationPage.tsx
@@ -1,75 +1,14 @@
-import { useEffect, type ChangeEvent } from 'react'
-import {useCharacterStore, useLocationStore} from '@/stores'
-import { useInfiniteScroll } from '@/common/hooks'
-import { ErrorMessage, Loader, PageTitle } from '@/common/components'
+import { EntityListPage } from '@/common/components'
+import { useLocationStore } from '@/stores'
 import { LocationsList } from './LocationsList/LocationsList'
-import s from './LocationPage.module.css'
 
-export const LocationPage = () => {
-  const {
-    locations,
-    info,
-    isLoading,
-    error,
-    searchQuery,
-    fetchLocations,
-    fetchNextPage,
-    setSearchQuery,
-    setScrollPosition,
-  } = useLocationStore()
-
-  const observerRef = useInfiniteScroll({
-    hasMore: !!info.next && searchQuery.trim() === '',
-    loadMore: fetchNextPage,
-    isLoading,
-  })
-
-  useEffect(() => {
-    const state = useLocationStore.getState()
-
-    if (state.locations.length === 0 && state.searchQuery === '') {
-      fetchLocations('/location')
-    }
-
-    if (state.scrollPosition > 0) {
-      setTimeout(() => {
-        window.scrollTo({ top: state.scrollPosition, behavior: 'auto' })
-      }, 0)
-    }
-
-    return () => {
-      useCharacterStore.getState().setScrollPosition(window.scrollY)
-    }
-  }, [fetchLocations])
-
-  const searchHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.currentTarget.value
-    setSearchQuery(value)
-    setScrollPosition(0)
-    window.scrollTo(0, 0)
-    fetchLocations(`/location?name=${value}`)
-  }
-
-  return (
-    <div className={'pageContainer'}>
-      <PageTitle
-        colorType={'locations'}
-        title={'Location Multiverse'}
-        searchQuery={searchQuery}
-        onSearch={searchHandler}
-        placeholder={'Search across locations... (e.g., Earth, Post-Apocalyptic Earth)'}
-      />
-
-      {!isLoading && error && <ErrorMessage error={error} />}
-
-      {locations.length > 0 && <LocationsList locations={locations} />}
-
-      {isLoading && locations.length > 0 && <Loader colorType={'locations'} text={'Loading more locations...'} />}
-      {isLoading && locations.length === 0 && (
-        <Loader colorType={'locations'} text={'Scanning the multiverse for locations...'} />
-      )}
-
-      {!isLoading && !!info.next && <div ref={observerRef} className={s.infiniteScrollAnchor} />}
-    </div>
-  )
-}
+export const LocationPage = () => (
+  <EntityListPage
+    store={useLocationStore()}
+    endpoint={'/location'}
+    colorType={'locations'}
+    title={'Location Multiverse'}
+    placeholder={'Search across locations... (e.g., Earth, Post-Apocalyptic Earth)'}
+    renderList={(locations) => <LocationsList locations={locations} />}
+  />
+)
