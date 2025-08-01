6919c28c0a9a96e18a5ab6781a8171e334af8f11
Kanin Gleb <gleb.kanin@gmail.com>
2025-08-01 22:21:22 +0300

    feat(CharacterPage): implement usePageData in CharacterPage

--- src/pages/CharacterPage/CharacterPage.tsx
+++ src/pages/CharacterPage/CharacterPage.tsx
@@ -1,23 +1,12 @@
-import { type ChangeEvent, useEffect } from 'react'
 import { Link } from 'react-router'
 import { useCharacterStore } from '@/stores'
-import { useInfiniteScroll } from '@/common/hooks'
+import { useInfiniteScroll, usePageData } from '@/common/hooks'
 import { ErrorMessage, Loader, PageTitle } from '@/common/components'
 import { PATH } from '@/common/data/paths.ts'
 import s from './CharacterPage.module.css'
 
 export const CharacterPage = () => {
-  const {
-    characters,
-    info,
-    isLoading,
-    error,
-    searchQuery,
-    fetchCharacters,
-    fetchNextPage,
-    setSearchQuery,
-    setScrollPosition,
-  } = useCharacterStore()
+  const { items, info, isLoading, error, searchQuery, fetchNextPage } = useCharacterStore()
 
   const observerRef = useInfiniteScroll({
     hasMore: !!info.next && searchQuery.trim() === '',
@@ -25,31 +14,10 @@ export const CharacterPage = () => {
     isLoading,
   })
 
-  useEffect(() => {
-    const state = useCharacterStore.getState()
-
-    if (state.characters.length === 0 && state.searchQuery === '') {
-      fetchCharacters('/character')
-    }
-
-    if (state.scrollPosition > 0) {
-      setTimeout(() => {
-        window.scrollTo({ top: state.scrollPosition, behavior: 'auto' })
-      }, 0)
-    }
-
-    return () => {
-      useCharacterStore.getState().setScrollPosition(window.scrollY)
-    }
-  }, [fetchCharacters])
-
-  const searchHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.currentTarget.value
-    setSearchQuery(value)
-    setScrollPosition(0)
-    window.scrollTo(0, 0)
-    fetchCharacters(`/character?name=${value}`)
-  }
+  const { searchHandler } = usePageData({
+    store: useCharacterStore(),
+    endpoint: '/character',
+  })
 
   return (
     <div className={'pageContainer'}>
@@ -63,9 +31,9 @@ export const CharacterPage = () => {
 
       {!isLoading && error && <ErrorMessage error={error} />}
 
-      {characters.length > 0 && (
+      {items.length > 0 && (
         <div className={s.characters}>
-          {characters.map((character) => (
+          {items.map((character) => (
             <Link className={s.characterLink} key={character.id} to={`${PATH.Characters}/${character.id}`}>
               <div className={s.card}>
                 <img className={s.avatar} src={character.image} alt={`${character.name} avatar`} />
@@ -78,8 +46,8 @@ export const CharacterPage = () => {
         </div>
       )}
 
-      {isLoading && characters.length > 0 && <Loader colorType={'characters'} text={'Loading more characters...'} />}
-      {isLoading && characters.length === 0 && <Loader colorType={'characters'} />}
+      {isLoading && items.length > 0 && <Loader colorType={'characters'} text={'Loading more characters...'} />}
+      {isLoading && items.length === 0 && <Loader colorType={'characters'} />}
       {!isLoading && !!info.next && <div ref={observerRef} className={s.infiniteScrollAnchor} />}
     </div>
   )
