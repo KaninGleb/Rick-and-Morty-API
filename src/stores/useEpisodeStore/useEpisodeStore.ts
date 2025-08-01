45d5fd5d4e7b73a945df4fd680af57a99b75f6cf
Kanin Gleb <gleb.kanin@gmail.com>
2025-08-01 22:27:19 +0300

    refactor(useEpisodeStore): replace manual logic with createEntityStore

--- src/stores/useEpisodeStore/useEpisodeStore.ts
+++ src/stores/useEpisodeStore/useEpisodeStore.ts
@@ -1,64 +1,69 @@
-import { create } from 'zustand'
-import type { EpisodeZustandStore } from './useEpisodeStore.types.ts'
+import { createEntityStore } from '@/stores'
 import { api } from '@/pages/api'
-import type { AxiosError } from 'axios'
 
-export const useEpisodeStore = create<EpisodeZustandStore>((set, get) => ({
-  episodes: [],
-  info: {
-    count: 0,
-    pages: 0,
-    next: null,
-    prev: null,
-  },
-  searchQuery: '',
-  isLoading: false,
-  error: null,
-  scrollPosition: 0,
+export const useEpisodeStore = createEntityStore(api.getEpisodes, '/episode')
 
-  setSearchQuery: (query) => {
-    set({ searchQuery: query })
-  },
-
-  setScrollPosition: (position) => {
-    set({ scrollPosition: position })
-  },
-
-  fetchEpisodes: async (url = '/episode') => {
-    set({ isLoading: true, error: null })
-    try {
-      const res = await api.getEpisodes(url)
-      set({
-        episodes: res.data.results,
-        info: res.data.info,
-        isLoading: false,
-      })
-    } catch (e) {
-      const error = e as AxiosError
-      set({
-        error: error.message || 'Unknown error',
-        isLoading: false,
-        episodes: [],
-        info: { count: 0, pages: 0, next: null, prev: null },
-      })
-    }
-  },
-
-  fetchNextPage: async () => {
-    const { info, episodes } = get()
-    if (!info.next) return
-
-    set({ isLoading: true })
-    try {
-      const response = await api.getEpisodes(info.next)
-      set({
-        episodes: [...episodes, ...response.data.results],
-        info: response.data.info,
-        isLoading: false,
-      })
-    } catch (e) {
-      const message = (e as Error)?.message || 'Unknown error'
-      set({ error: message, isLoading: false })
-    }
-  },
-}))
+// import { create } from 'zustand'
+// import type { EpisodeZustandStore } from './useEpisodeStore.types.ts'
+// import { api } from '@/pages/api'
+// import type { AxiosError } from 'axios'
+//
+// export const useEpisodeStore = create<EpisodeZustandStore>((set, get) => ({
+//   episodes: [],
+//   info: {
+//     count: 0,
+//     pages: 0,
+//     next: null,
+//     prev: null,
+//   },
+//   searchQuery: '',
+//   isLoading: false,
+//   error: null,
+//   scrollPosition: 0,
+//
+//   setSearchQuery: (query) => {
+//     set({ searchQuery: query })
+//   },
+//
+//   setScrollPosition: (position) => {
+//     set({ scrollPosition: position })
+//   },
+//
+//   fetchEpisodes: async (url = '/episode') => {
+//     set({ isLoading: true, error: null })
+//     try {
+//       const res = await api.getEpisodes(url)
+//       set({
+//         episodes: res.data.results,
+//         info: res.data.info,
+//         isLoading: false,
+//       })
+//     } catch (e) {
+//       const error = e as AxiosError
+//       set({
+//         error: error.message || 'Unknown error',
+//         isLoading: false,
+//         episodes: [],
+//         info: { count: 0, pages: 0, next: null, prev: null },
+//       })
+//     }
+//   },
+//
+//   fetchNextPage: async () => {
+//     const { info, episodes } = get()
+//     if (!info.next) return
+//
+//     set({ isLoading: true })
+//     try {
+//       const response = await api.getEpisodes(info.next)
+//       set({
+//         episodes: [...episodes, ...response.data.results],
+//         info: response.data.info,
+//         isLoading: false,
+//       })
+//     } catch (e) {
+//       const message = (e as Error)?.message || 'Unknown error'
+//       set({ error: message, isLoading: false })
+//     }
+//   },
+// }))
