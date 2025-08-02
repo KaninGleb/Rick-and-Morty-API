07af6e6d48a1472ee217061bb003107d5c88ac37
Kanin Gleb <gleb.kanin@gmail.com>
2025-08-02 18:35:59 +0300

    refactor(LocationPage): add locationPageConfig

--- src/pages/LocationPage/LocationPage.tsx
+++ src/pages/LocationPage/LocationPage.tsx
@@ -1,15 +1,17 @@
-import { EntityListPage } from '@/common/components'
+import { EntityListPage, type EntityPageConfig } from '@/common/components'
 import { useLocationStore } from '@/stores'
 import { LocationsList } from './LocationsList/LocationsList'
 
+const locationPageConfig: EntityPageConfig = {
+  endpoint: '/location',
+  title: 'Location Multiverse',
+  placeholder: 'Search across locations... (e.g., Earth, Post-Apocalyptic Earth)',
+}
+
 export const LocationPage = () => (
   <EntityListPage
-    pageKey={'location'}
+    config={locationPageConfig}
     store={useLocationStore()}
-    endpoint={'/location'}
-    colorType={'locations'}
-    title={'Location Multiverse'}
-    placeholder={'Search across locations... (e.g., Earth, Post-Apocalyptic Earth)'}
     renderList={(locations) => <LocationsList locations={locations} />}
   />
 )
