562141654fb8e3b84414f4acaf8858bb74835e3a
Kanin Gleb <gleb.kanin@gmail.com>
2025-08-02 19:22:43 +0300

    feat(Header): improve Header

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,6 +1,8 @@
-import { NavLink } from 'react-router'
-import { logo } from '@/assets'
+import { useLocation, NavLink } from 'react-router'
+import type { CSSProperties } from 'react'
 import { PATH } from '@/common/data/paths.ts'
+import { logo } from '@/assets'
+import { getActiveColorVar } from '@/common'
 import s from './Header.module.css'
 
 const navItems = [
@@ -11,8 +13,15 @@ const navItems = [
 ]
 
 export const Header = () => {
+  const location = useLocation()
+  const activeColor = getActiveColorVar(location.pathname)
+
+  const headerStyle = {
+    '--active-nav-color': activeColor,
+  } as CSSProperties
+
   return (
-    <header className={s.header}>
+    <header className={s.header} style={headerStyle}>
       <div className={s.container}>
         <NavLink to={PATH.Home} className={s.logoLink} aria-label={'Go to home page'}>
           <img className={s.logo} src={logo} alt={'Site logo'} />
@@ -21,8 +30,12 @@ export const Header = () => {
           <ul className={s.navList}>
             {navItems.map(({ path, label }) => (
               <li key={path}>
-                <NavLink to={path} className={({ isActive }) => `${s.headerLink} ${isActive ? s.activeLink : ''}`}>
-                  {label}
+                <NavLink
+                  to={path}
+                  data-label={label}
+                  className={({ isActive }) => `${s.headerLink} ${isActive ? s.activeLink : ''}`}
+                >
+                  <span className={s.labelText}>{label}</span>
                 </NavLink>
               </li>
             ))}
